ec6abff2ff88645090d4445660f121ac1b8ab6c3
Lucas Correa <correalucas1905@gmail.com>
2023-11-02 11:05:01 -0300

    feat(home): add tech stack

--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -1,38 +1,56 @@
 import { Playfair_Display } from "next/font/google";
 import { FaReact } from "react-icons/fa";
-import Image from "next/image";
+import { DiHtml5, DiCss3, DiJsBadge } from "react-icons/di";
+import {
+  SiNextdotjs,
+  SiVite,
+  SiTailwindcss,
+  SiRedux,
+  SiNodedotjs,
+  SiExpress,
+  SiPostgresql,
+  SiJest,
+  SiTestinglibrary,
+  SiVitest,
+} from "react-icons/si";
+
 const playFair = Playfair_Display({ subsets: ["latin"] });
 const LOGOS = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Next",
-  "Vite",
-  "Tailwind",
-  "Redux",
-  "Node",
-  "Express",
-  "PostgreSQL",
-  "Jest",
+  { name: "HTML", logo: <DiHtml5 className="mr-2 text-xl" /> },
+  { name: "CSS", logo: <DiCss3 className="mr-2 text-xl" /> },
+  { name: "JavaScript", logo: <DiJsBadge className="mr-2 " /> },
+  { name: "React", logo: <FaReact className="mr-2" /> },
+  { name: "Next.js", logo: <SiNextdotjs className="mr-2" /> },
+  { name: "Vite", logo: <SiVite className="mr-2" /> },
+  { name: "Tailwind", logo: <SiTailwindcss className="mr-2" /> },
+  { name: "Redux", logo: <SiRedux className="mr-2" /> },
+  { name: "Node", logo: <SiNodedotjs className="mr-2" /> },
+  { name: "Express", logo: <SiExpress className="mr-2" /> },
+  { name: "PostgreSQL", logo: <SiPostgresql className="mr-2" /> },
+  { name: "Jest", logo: <SiJest className="mr-2" /> },
+  { name: "Vitest", logo: <SiVitest className="mr-2" /> },
+  { name: "Testing Library", logo: <SiTestinglibrary className="mr-2" /> },
 ];
+
 const TechStack = () => {
   return (
-    <section>
+    <section className="select-none  border-b-[1px] border-neutral-600 pb-4">
       <h2
-        className={` ${playFair.className} text-xl  tracking-tighter text-neutral-300 `}
+        className={` ${playFair.className} text-xl  tracking-tighter text-neutral-300 border-b-[1px] border-neutral-600 mb-4 `}
       >
-        Tecnologias
+        tecnologias
       </h2>
-      <a className="border  border-neutral-700 mx-1  bg-neutral-800 rounded p-2  inline-flex items-center leading-4  text-neutral-100 no-underline">
-        <FaReact className="mr-1 text-xl" />
-        React
-      </a>
-      <article className="flex flex-wrap gap-4 my-8">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
         {LOGOS.map((logo, index) => (
-          <Image src={`/sliderImages/${logo}-logo.svg`} alt="" width={36} height={36} key={index}/>
+          <a
+            key={index}
+            className="border  border-neutral-700 mx-1  font-thin bg-neutral-800 rounded p-2  inline-flex items-center leading-4  text-neutral-100 no-underline"
+          >
+            {logo.logo}
+            <p className="truncate">{logo.name}</p>
+          </a>
         ))}
-      </article>
+      </div>
     </section>
   );
 };
