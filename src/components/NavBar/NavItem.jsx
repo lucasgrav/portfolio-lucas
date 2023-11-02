b01627494517d6d5d35e8ee144971483667d4005
Lucas Correa <correalucas1905@gmail.com>
2023-11-02 11:23:39 -0300

    fix(home): navbar item color

--- src/components/NavBar/NavItem.jsx
+++ src/components/NavBar/NavItem.jsx
@@ -9,7 +9,7 @@ const NavItem = ({ path, name }) => {
   return (
     <Link href={path}>
       <span
-        className={`${playFair.className} transition-all relative py-2 px-2  ${
+        className={`${playFair.className} transition-all relative py-1 px-2  ${
           booleanPath ? "text-[#ffffff]" : "text-neutral-500"
         } hover:text-neutral-200`}
       >
@@ -17,7 +17,7 @@ const NavItem = ({ path, name }) => {
         <div
           className={`absolute h-[1px] top-7 mx-2 ${
             booleanPath && "inset-0"
-          }   bg-neutral-800 z-[-1] bg-gradient-to-r from-transparent to-neutral-900`}
+          }   bg-neutral-600 z-[-1] bg-gradient-to-r from-transparent to-neutral-900`}
         ></div>
       </span>
     </Link>
